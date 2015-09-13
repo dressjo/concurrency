9125f55eddb25b8b86b29032b55f4a1b078fffcf
Johannes Dressie <johannes.dressie@gmail.com>
2015-09-13 13:40:59 +0200

    2015-09-13

--- concurrency-main/src/main/java/ch5_5/TestHarness.java
+++ concurrency-main/src/main/java/ch5_5/TestHarness.java
@@ -17,7 +17,7 @@ public class TestHarness {
 					try {
 						startGate.await();
 						try {
-							task.run();
+							task.run();		
 						} finally {
 							endGate.countDown();
 						}
@@ -26,7 +26,7 @@ public class TestHarness {
 						
 					}
 				}
-				
+					
 			};
 			t.start();
 		}
